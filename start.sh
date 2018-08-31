a238c091356b6313c27f8ab2f5bd13671081d6b0
takara <taka2063jp@gmail.com>
2018-08-31 09:57:44 +0900

    初期ファイル

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eux
+
+cp /server.properties /data/server.properties
+
+wget -q -O /data/install.sh https://get.pmmp.io
+chmod 0755 /data/install.sh
+wget -q -O /data/php-linux-x86-64.tar.gz https://jenkins.pmmp.io/job/PHP-7.2-Linux-x86_64/lastSuccessfulBuild/artifact/PHP_Linux-x86_64.tar.gz
+
+cd /data
+#./install.sh -r -v development
+./install.sh -r -v stable
+tar -zxf ./php-linux-x86-64.tar.gz --no-same-owner
+rm -f /data/install.sh /data/php-linux-x86-64.tar.gz
+
+#cp /pocketmine.yml /data/
+
+# start pocketmine
+exec ./start.sh
